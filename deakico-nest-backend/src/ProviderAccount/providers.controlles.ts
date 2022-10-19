803eb5144ba646ea3f93421b516c6aca79610efb
javieridv <javier.delvalle2@upr.edu>
2022-10-19 11:11:43 -0400

    provider get request

--- deakico-nest-backend/src/ProviderAccount/providers.controlles.ts
+++ deakico-nest-backend/src/ProviderAccount/providers.controlles.ts
@@ -1,5 +1,4 @@
-import { Controller, Post, Body, Get, Param, Put, Delete } from "@nestjs/common";
-import { throws } from "assert";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { DeleteResult, UpdateResult } from "typeorm";
 import { ProviderAccount } from "./providers.interface";
@@ -7,34 +6,44 @@ import { ProviderAccountService } from "./providers.service";
 
 @Controller('providers')
 export class ProviderAccountController {
-    constructor(private readonly providersService: ProviderAccountService) {}
+  constructor(private readonly providersService: ProviderAccountService) { }
 
-    @Post()
-    insertProvider(@Body() provider: ProviderAccount): Observable<ProviderAccount> {
-        return this.providersService.insertProvider(provider);
-    }
+  @Post()
+  insertProvider(@Body() provider: ProviderAccount): Observable<ProviderAccount> {
+    return this.providersService.insertProvider(provider);
+  }
 
-    @Get()
-    getAllProviders(): Observable<ProviderAccount[]>{
-        return this.providersService.getAllProviders();
-    }
+  @Get()
+  getAllProviders(): Observable<ProviderAccount[]> {
+    return this.providersService.getAllProviders();
+  }
 
-    @Get(':pa_id')
-    getProvider(@Param('pa_id') pa_Id: number,): Observable<ProviderAccount> {
-        return this.providersService.getProvider(pa_Id);
-    }
+  @Get(':pa_id')
+  getProvider(@Param('pa_id') pa_Id: number,): Observable<ProviderAccount> {
+    return this.providersService.getProvider(pa_Id);
+  }
 
-    @Put(':pa_id')
-    updateProvider(
-        @Param('pa_id') pa_Id: number,
-        @Body() provider: ProviderAccount,
-    ): Observable<UpdateResult> {
-        return this.providersService.updateProvider(pa_Id, provider);
-    }
+  /**
+   * Fetches list of providers that belong to given category or are null
+   * @param providerCat string of 'null' or of category of provider
+   * @returns all providers that are part of given category or null
+   */
+  @Get('category/:pa_category')
+  getProviderCategory(@Param('pa_category') providerCat: string) {
+    return this.providersService.getProviderCategory(providerCat);
+  }
 
-    @Delete(':pa_id')
-    deleteProvider(
-        @Param('pa_id') pa_Id: number,): Observable<DeleteResult> {
-            return this.providersService.deleteProvider(pa_Id);
-    }
+  @Put(':pa_id')
+  updateProvider(
+    @Param('pa_id') pa_Id: number,
+    @Body() provider: ProviderAccount,
+  ): Observable<UpdateResult> {
+    return this.providersService.updateProvider(pa_Id, provider);
+  }
+
+  @Delete(':pa_id')
+  deleteProvider(
+    @Param('pa_id') pa_Id: number,): Observable<DeleteResult> {
+    return this.providersService.deleteProvider(pa_Id);
+  }
 }
