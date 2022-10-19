3590ad806a4caa7abde400bc9256c902aaeda354
javieridv <javier.delvalle2@upr.edu>
2022-10-18 21:37:12 -0400

    Item get funcitons

--- deakico-nest-backend/src/Item/items.controllers.ts
+++ deakico-nest-backend/src/Item/items.controllers.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param, Put, Delete } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param, Put, Delete, HttpException, HttpStatus } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { DeleteResult, UpdateResult } from "typeorm";
 import { Item } from "./items.interface";
@@ -6,34 +6,59 @@ import { ItemsService } from "./items.service";
 
 @Controller('items')
 export class ItemsController {
-    constructor(private readonly itemsService: ItemsService) {}
+  constructor(private readonly itemsService: ItemsService) { }
 
-    @Post()
-    insertItem(@Body() item: Item): Observable<Item> {
-        return this.itemsService.insertItem(item);
+  @Get()
+  getAllItems(): Observable<Item[]> {
+    return this.itemsService.getAllItems();
+  }
+
+  @Get('/id/:i_id')
+  getItem(@Param('i_id') itemId: number,): Observable<Item> {
+    return this.itemsService.getItem(itemId);
+  }
+
+  @Get('type/:i_type')
+  getItemByType(@Param('i_type') itemType: string,): Observable<Item[]> {
+    const types = ['product', 'service'];
+    if (types.includes(itemType)) {
+      return this.itemsService.getItemByType(itemType);
+    } else {
+      throw new HttpException('Not a type', HttpStatus.BAD_REQUEST)
     }
+  }
 
-    @Get()
-    getAllItems(): Observable<Item[]>{
-        return this.itemsService.getAllItems();
-    }
+  @Get('category')
+  getItemCategories(): Promise<Partial<Item[]>> {
+    return this.itemsService.getItemCategories();
+  }
 
-    @Get(':i_id')
-    getItem(@Param('i_id') itemId: number,): Observable<Item> {
-        return this.itemsService.getItem(itemId);
-    }
+  @Get('category/:i_category')
+  getItemByCategory(@Param('i_category') itemCategory: string,): Observable<Item[]> {
+    return this.itemsService.getItemByCategory(itemCategory);
+  }
 
-    @Put(':i_id')
-    updateItem(
-        @Param('i_id') itemId: number,
-        @Body() item: Item,
-    ): Observable<UpdateResult> {
-        return this.itemsService.updateItem(itemId, item);
-    }
+  @Get('provider/:pa_id')
+  getItemOfProvider(@Param('pa_id') itemProvider: number,): Observable<Item[]> {
+    return this.itemsService.getItemOfProvider(itemProvider);
+  }
 
-    @Delete(':i_id')
-    deleteItem(
-        @Param('i_id') itemId: number,): Observable<DeleteResult> {
-            return this.itemsService.deleteItem(itemId);
-        }
+  @Post()
+  insertItem(@Body() item: Item): Observable<Item> {
+    return this.itemsService.insertItem(item);
+  }
+
+  @Put('/id/:i_id')
+  updateItem(
+    @Param('i_id') itemId: number,
+    @Body() item: Item,
+  ): Observable<UpdateResult> {
+    return this.itemsService.updateItem(itemId, item);
+  }
+
+  @Delete('/id/:i_id')
+  deleteItem(
+    @Param('i_id') itemId: number,): Observable<DeleteResult> {
+    return this.itemsService.deleteItem(itemId);
+  }
 }
