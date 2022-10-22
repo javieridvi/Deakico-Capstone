8c8ba2975103f5c4d07dea38a2e71010010b44e1
josejavi3009 <jose.vazquez38@upr.edu>
2022-10-22 19:20:08 -0400

    added arbitrary categories list

--- deakico-next-frontend/pages/home/index.js
+++ deakico-next-frontend/pages/home/index.js
@@ -1,10 +1,31 @@
 import { Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import Banner from "../../deakicomponents/Home/Banner";
 import Categories from "../../deakicomponents/Home/Categories";
+import ToggleSection from "../../deakicomponents/Home/ToggleSection";
 import TopProviders from "../../deakicomponents/Home/TopProviders";
+import itemService from "../../services/item.service"
 
+function Home(){
+  
+  // const [categories, setCategories] = useState([]);
 
-export default function Home(){
+  // function callGetItemCategories() {
+  //   itemService.getItemCategories()
+  //   .then((res) => {setCategories(res.data.map((e)=> {return e.i_category}))}).catch((err) => console.log(err));
+  // };
+
+  // useEffect(() => {
+  //   callGetItemCategories();
+  // }, []);
+
+  const categories = [
+    "Hair",
+    "Pastry",
+    "Food",
+    "Clothing",
+    "Other"
+  ];
 
   return (
     <Stack
@@ -20,10 +41,11 @@ export default function Home(){
     }}
     >
       <Banner/>
-      <Typography variant="h4">Service/Provider Toggle</Typography>
+      <ToggleSection/>
       <TopProviders/>
-      <Categories/>
-      <Typography variant="h2">Extra info</Typography>
+      <Categories categoryList={categories}/>
+      
     </Stack>
   );
 };
+export default Home;
