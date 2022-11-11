05720c863e9348be3f7f934363f2a781cd557737
josejavi3009 <jose.vazquez38@upr.edu>
2022-11-11 19:59:24 -0400

    updated: imports, vercel.json, and linting

--- deakico-nest-backend/src/UserAccount/users.entity.ts
+++ deakico-nest-backend/src/UserAccount/users.entity.ts
@@ -3,44 +3,54 @@ import { LikeEntity } from '../Likes/likes.entity';
 import { ProviderAccountEntity } from '../ProviderAccount/providers.entity';
 import { RequestEntity } from '../Request/requests.entity';
 import { ReviewEntity } from '../Review/reviews.entity';
-import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+  OneToMany,
+  Unique,
+} from 'typeorm';
+import { type } from 'os';
 
 @Entity('User Account')
+@Unique(['username', 'email'])
 export class UserAccountEntity {
-    @PrimaryGeneratedColumn()
-    u_id: number;
+  @PrimaryGeneratedColumn()
+  u_id: number;
 
-    @Column()
-    u_firstname: string;
-    
-    @Column()       //find ways to set a default date of "now"
-    u_lastname: string; 
-    
-    @Column()     //this attribute might be redundant
-    email: string;  
+  @Column()
+  u_firstname: string;
 
-    @Column()
-    username: string;
+  @Column() //find ways to set a default date of "now"
+  u_lastname: string;
 
-    @Column() //consider {select: false} for better security
-    password: string;  
+  @Column() //this attribute might be redundant
+  email: string;
 
-    @OneToMany(type => FollowEntity, follow => follow.user_follows)
-    follows: FollowEntity[];
+  @Column()
+  username: string;
 
-    @OneToMany(type => LikeEntity, like => like.user_likes )
-    likes: LikeEntity[];
+  @Column() //consider {select: false} for better security
+  password: string;
 
-    @OneToOne(type => ProviderAccountEntity)
-    @JoinColumn({name: 'pa_id'})
-    user_pa: ProviderAccountEntity[];
+  @OneToMany((type) => FollowEntity, (follow) => follow.user_follows)
+  follows: FollowEntity[];
 
-    @Column({ type: 'int', nullable: true})
-    pa_id: number;
+  @OneToMany((type) => LikeEntity, (like) => like.user_likes)
+  likes: LikeEntity[];
 
-    @OneToMany(type => ReviewEntity, rev => rev.user)
-    reviews: ReviewEntity[];
+  @OneToOne((type) => ProviderAccountEntity)
+  @JoinColumn({ name: 'pa_id' })
+  user_pa: ProviderAccountEntity[];
 
-    @OneToMany(type => RequestEntity, req => req.user)
-    requests: RequestEntity[];
+  @Column({ type: 'int', nullable: true })
+  pa_id: number;
+
+  @OneToMany((type) => ReviewEntity, (rev) => rev.user)
+  reviews: ReviewEntity[];
+
+  @OneToMany((type) => RequestEntity, (req) => req.user)
+  requests: RequestEntity[];
 }
