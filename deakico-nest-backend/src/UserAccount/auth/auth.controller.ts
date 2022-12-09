e9ee5f3fedff7e000cee9e1768d469bf65586d71
josejavi3009 <jose.vazquez38@upr.edu>
2022-12-09 00:46:51 -0400

    Setting page implemented. Still needs data vaidation.

--- deakico-nest-backend/src/UserAccount/auth/auth.controller.ts
+++ deakico-nest-backend/src/UserAccount/auth/auth.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put, Request, UseGuards } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common/enums';
+import { HttpException } from '@nestjs/common/exceptions';
 import { map, Observable } from 'rxjs';
+import { UpdateResult } from 'typeorm';
 import { UserAccount } from '../users.interface';
 import { AuthService } from './auth.service';
+import { JwtGuard } from './guards/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -15,8 +19,17 @@ export class AuthController {
 
   @Post('login')
   login(@Body() user: UserAccount): Observable<{ token: string }> {
-    return this.authService
+
+      return this.authService
       .login(user)
       .pipe(map((jwt: string) => ({ token: jwt })));
+  }
+
+  @UseGuards(JwtGuard)
+  @Put('update-password')
+  async updatePassword(@Request() req: any, @Body() body: UserAccount ): Promise<Observable<UpdateResult>> { 
+    return this.authService.updatePassword(req.user.u_id, body.password).catch((err) => {
+      throw new HttpException('Error while updating password!', HttpStatus.UNAUTHORIZED);
+    });
   }
 }
