adbcd52158741e99b2bdffdf8a8ba087d8ab7fe2
Jose Vazquez <31424499+josejavi3009@users.noreply.github.com>
2022-12-10 12:25:40 -0400

    Update next.config.js

--- deakico-next-frontend/next.config.js
+++ deakico-next-frontend/next.config.js
@@ -1,57 +1,57 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// // This file sets a custom webpack configuration to use your Next.js app
+// // with Sentry.
+// // https://nextjs.org/docs/api-reference/next.config.js/introduction
+// // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs');
+// const { withSentryConfig } = require('@sentry/nextjs');
 
-const moduleExports = {
-  // Your existing module.exports
+// const moduleExports = {
+//   // Your existing module.exports
 
-  env: {
-    API_URL: process.env.API_URL,
-  },
+//   env: {
+//     API_URL: process.env.API_URL,
+//   },
 
-  sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
-    hideSourceMaps: true,
-  },
-};
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
-
-// module.exports = {
-//   async headers() {
-//     return [
-//       {
-//         // matching all API routes
-//         source: "/api/:path*",
-//         headers: [
-//           { key: "Access-Control-Allow-Credentials", value: "true" },
-//           { key: "Access-Control-Allow-Origin", value: "*" },
-//           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-//         ]
-//       }
-//     ]
-//   }
+//   sentry: {
+//     // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+//     // for client-side builds. (This will be the default starting in
+//     // `@sentry/nextjs` version 8.0.0.) See
+//     // https://webpack.js.org/configuration/devtool/ and
+//     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+//     // for more information.
+//     hideSourceMaps: true,
+//   },
 // };
+
+// const sentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, org, project, authToken, configFile, stripPrefix,
+//   //   urlPrefix, include, ignore
+
+//   silent: true, // Suppresses all logs
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// };
+
+// // Make sure adding Sentry options is the last code to run before exporting, to
+// // ensure that your source maps include changes from all other Webpack plugins
+// module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/https://hku4e8x349.execute-api.us-east-1.amazonaws.com/dev/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+};
