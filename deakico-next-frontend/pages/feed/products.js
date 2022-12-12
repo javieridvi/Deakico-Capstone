207da6262916b300cf4e60b817a6047ea8c6ad89
javieridv <javier.delvalle2@upr.edu>
2022-12-12 02:10:13 -0400

    fixing stuff

--- deakico-next-frontend/pages/feed/products.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import FeedFunc from '../feed/index';
-
-
-export default function Products() {
-  //This function serves as a redirect for the navbar products button.
-  return <FeedFunc type={'products'}/>
-}
